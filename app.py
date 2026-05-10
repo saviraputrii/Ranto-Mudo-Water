8568d88de3ab772ad74c770fd072c88df92f1511
saviraputrii <saviraputripramudita@gmail.com>
2026-05-10 14:22:34 +0700

    app.py

--- app.py
+++ app.py
@@ -99,36 +99,35 @@ else:
     if menu == "Dashboard":
        st.title("💧 Dashboard💧")
        st.markdown("""
-            <div style="
-            background-color:#f5f7fa;
-            padding:25px;
-            border-radius:15px;
-            box-shadow:0 4px 10px rgba(0,0,0,0.1);
-            color:#333333;
-            ">
+       <div style="
+       background-color:#f5f7fa;
+       padding:25px;
+       border-radius:15px;
+       box-shadow:0 4px 10px rgba(0,0,0,0.1);
+       color:#333333;
+       ">  
+       <h2 style="color:#1f4e79;">📌 Informasi Usaha</h2>
             
-            <h2 style="color:#1f4e79;">📌 Informasi Usaha</h2>
-            
-    <p style="text-align:justify; font-size:16px; color:#333333;">
+       <p style="text-align:justify; font-size:16px; color:#333333;">
     
-    Selamat datang di <b>Ranto Mudo Water</b>, aplikasi manajemen  air galon isi ulang yang dirancang untuk membantu proses operasional usaha menjadi efisien dan terorganisir. Sistem ini dikembangkan untuk mempermudah pengelolaan data pelanggan, monitoring stok galon, pencatatan transaksi penjualan, pengelolaan tagihan, hingga penyusunan laporan usaha secara otomatis dalam satu platform.
+       Selamat datang di <b>Ranto Mudo Water</b>, aplikasi manajemen  air galon isi ulang yang dirancang untuk membantu proses operasional usaha menjadi efisien dan terorganisir. Sistem ini dikembangkan untuk mempermudah pengelolaan data pelanggan, monitoring stok galon, pencatatan transaksi penjualan, pengelolaan tagihan, hingga penyusunan laporan usaha secara otomatis dalam satu platform.
     
-    </p>
+       </p>
     
-    <p style="text-align:justify; font-size:16px; color:#333333;">
+       <p style="text-align:justify; font-size:16px; color:#333333;">
     
-    Dengan tampilan yang sederhana namun profesional, aplikasi ini membantu meningkatkan efisiensi pelayanan, meminimalkan kesalahan pencatatan data, serta mempermudah pemilik usaha dalam memantau kondisi bisnis secara real-time. Seluruh informasi tersusun secara rapi sehingga proses pengambilan keputusan dapat dilakukan dengan lebih cepat, tepat, dan akurat.
+       Dengan tampilan yang sederhana namun profesional, aplikasi ini membantu meningkatkan efisiensi pelayanan, meminimalkan kesalahan pencatatan data, serta mempermudah pemilik usaha dalam memantau kondisi bisnis secara real-time. Seluruh informasi tersusun secara rapi sehingga proses pengambilan keputusan dapat dilakukan dengan lebih cepat, tepat, dan akurat.
     
-    </p>
+       </p>
     
-    <p style="text-align:justify; font-size:16px; color:#333333;">
+       <p style="text-align:justify; font-size:16px; color:#333333;">
     
-    Melalui sistem digital berbasis <b>Streamlit</b>, pengelolaan depot air minum isi ulang dapat dilakukan dengan lebih praktis, aman, dan mudah diakses kapan saja sesuai kebutuhan operasional usaha.
+       Melalui sistem digital berbasis <b>Streamlit</b>, pengelolaan depot air minum isi ulang dapat dilakukan dengan lebih praktis, aman, dan mudah diakses kapan saja sesuai kebutuhan operasional usaha.
     
-    </p>
+       </p>
     
-    </div>
-    """, unsafe_allow_html=True)
+       </div>
+       """, unsafe_allow_html=True)
     
         col1, col2, col3, col4 = st.columns(4)
     
