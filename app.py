0ccf7cd3c2b64be7be001a0e838d3dcd1cf3121d
saviraputrii <saviraputripramudita@gmail.com>
2026-05-10 13:02:28 +0700

    app.py

--- app.py
+++ app.py
@@ -27,12 +27,7 @@ if not st.session_state.login:
 
         else:
             st.error("Username atau password salah!")
-  st.set_page_config(
-    page_title="Ranto Mudo Water - Aplikasi Galon Isi Ulang",
-    page_icon="💧💧",
-    layout="wide"
- )           
- 
+  
 # =========================
 # SIDEBAR
 # =========================
